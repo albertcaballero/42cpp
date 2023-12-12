1d258c9d138829c9be2b6610e6eedab50ca78b60
Albert Caballero <albert@Alberts-MacBook-Pro.local>
2023-12-12 19:05:34 +0100

    developed search function

--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -5,9 +5,31 @@ void add_contact()
 	return ;
 }
 
+void	display_all_contacts()
+{
+	std::string str = "hola!quetal!!moltbe";
+	std::cout << "index\t| first name\t| last name\t| nickname" << std::endl;
+	str.resize(10);
+	std::cout << str << "." << std::endl;
+}
+
 void search_contacts()
 {
-	return ;
+	std::string	index;
+	int			idx;
+	
+	display_all_contacts();
+	std::cin >> index;
+	std::cin.clear();
+	if (index.length() > 2){
+		std::cout << "out of scope, try again" << std::endl;
+		search_contacts();
+	}
+	idx = std::stoi(index);
+	if (idx > 7 || idx < 0){
+		std::cout << "out of scope, try again" << std::endl;
+		search_contacts();
+	}
 }
 
 int	main(void)
@@ -17,10 +39,13 @@ int	main(void)
 	{
 		std::cin >> input;
 		std::cin.clear();
-		if (input == "ADD")
-			std::cout << "gonna add this" << std::endl;
-		else if (input == "SEARCH")
-			std::cout << "gonna search that" << std::endl;
+		if (input == "ADD"){
+			std::cout << "what do you want to add?" << std::endl;
+		}
+		else if (input == "SEARCH"){
+			std::cout << "who do you want to search?" << std::endl;
+			search_contacts();
+		}
 		else if (input == "EXIT")
 		{
 			std::cout << "bye!!" << std::endl;
