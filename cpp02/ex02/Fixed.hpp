1254ec30ae4764592135e145d8c9be1ac29aa650
Albert Caballero Coll <alcaball@car13s1.42barcelona.com>
2024-02-15 18:28:44 +0100

    started cpp02ex02

---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,21 @@
+#include <iomanip>
+#include <iostream>
+
+class Fixed{
+	private:
+		int value;
+		static const int bits = 8;
+	public:
+		Fixed();
+		~Fixed();
+		Fixed &operator=(Fixed const& old);
+		Fixed(Fixed const& old);
+		Fixed(int const num);
+		Fixed(float const flnum);
+		float toFloat(void) const;
+		int toInt(void) const;
+		int getRawBits(void) const;
+		void setRawBits(int const raw); 
+};
+
+std::ostream &operator<<(std::ostream &out, Fixed const &fixed);
