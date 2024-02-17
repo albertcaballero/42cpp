6aeb1962ea3ffbde36a9c69dfb52be5d9692e91e
Albert Caballero <albert@Alberts-MacBook-Pro.local>
2024-02-17 10:19:37 +0100

    claptrap

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -1,13 +1,15 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap(): hp(10), ad(0), ep(10), name("Pedro"){
+	std::cout << "Constructed a ClapTrap (default)\n";
 }
 
 ClapTrap::ClapTrap(std::string _nam): hp(10), ad(0), ep(10), name(_nam) {
+	std::cout << "Constructed a ClapTrap (string)\n";
 }
 
 ClapTrap::~ClapTrap(){
-
+	std::cout << "Deconstructed a ClapTrap (dest)\n";
 }
 
 ClapTrap::ClapTrap(ClapTrap const& old)
@@ -16,10 +18,27 @@ ClapTrap::ClapTrap(ClapTrap const& old)
 	ad = old.ad;
 	ep = old.ep;
 	name = old.name;
+	std::cout << "Copied a ClapTrap (copy)\n";
 }
 
 ClapTrap ClapTrap::operator=(ClapTrap const& old){
+	std::cout << "Duplicated a ClapTrap (default)\n";
 	ClapTrap n(old);
 	return n;
 }
 
+void ClapTrap::attack(const std::string& target)
+{
+	std::cout << "ClapTrap " << name << " attacks " << target << ", causing "\
+		<< this->ad << " point of damage!\n";
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap took " << amount << " of damage!\n";
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	std::cout << "beepboop, ClapTrap is reparing for " << amount << " health points!\n";
+}
---
+++ cpp03/ex00/main.cpp
@@ -0,0 +1,12 @@
+#include "ClapTrap.hpp"
+
+int main(void)
+{
+	ClapTrap twoB("2B");
+	ClapTrap nines("9S");
+	ClapTrap adam("Adam");
+
+	// twoB.attack(adam.name);
+	//attacking or reparaing costs 1ep
+	//claptrap cannot do anything if no HP or no EP
+}
