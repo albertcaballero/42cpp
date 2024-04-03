b86fc495f40f207ceadd6ee1922476ae40e464c3
Albert caballero <albertcaballerocoll@gmail.com>
2024-04-03 14:26:17 +0200

    aaaaaa done

--- cpp04/ex03/src/main.cpp
+++ cpp04/ex03/src/main.cpp
@@ -6,94 +6,6 @@
 #include "MateriaSource.hpp"
 #include "ICharacter.hpp"
 
-void	subjectTest(void) {
-
-	std::cout << "===========================================" << std::endl;
-	std::cout << "Subject test" << std::endl;
-	std::cout << "===========================================" << std::endl;
-
-	IMateriaSource* src = new MateriaSource();
-
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
-
-	ICharacter* me = new Character("me");
-	AMateria* tmp;
-
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
-
-	ICharacter* bob = new Character("bob");
-	
-	me->use(0, *bob);
-	me->use(1, *bob);
-
-	delete bob;
-	delete me;
-	delete src;
-}
-
-void	extraTests(void) {
-
-	std::cout << "===========================================" << std::endl;
-	std::cout << "Extra test" << std::endl;
-	std::cout << "===========================================" << std::endl;
-
-	IMateriaSource *	src = new MateriaSource();
-	ICharacter *		mopo = new Character("Mopo");
-	AMateria *			tmp;
-
-	src->learnMateria(new Ice());
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
-	src->learnMateria(new Cure());
-	tmp = src->createMateria("ice");
-	mopo->equip(tmp);
-	mopo->equip(src->createMateria("icea"));
-
-	tmp = src->createMateria("ice");
-	mopo->equip(tmp);
-
-	tmp = src->createMateria("cure");
-	mopo->equip(tmp);
-
-	tmp = src->createMateria("ice");
-	mopo->equip(tmp);
-
-	tmp = src->createMateria("cure");
-	mopo->equip(tmp);
-	delete tmp;
-	mopo->unequip(0);
-
-	Character *	ziski = new Character("Ziski");
-	Character	ovi("Ovi");
-	
-	tmp = src->createMateria("cure");
-	ziski->equip(tmp);
-	tmp = src->createMateria("ice");
-	ziski->equip(tmp);
-	delete tmp;
-
-	std::cout << "+++++++ Copy ovi = ziski +++++++" << std::endl;
-	ovi = *ziski;
-
-	std::cout << "+++++++ Testing interactions +++++++" << std::endl;
-	mopo->use(0, *ziski);
-	mopo->use(-1, *ziski);
-	mopo->use(1, *ziski);
-	mopo->use(2, *ziski);
-	mopo->use(3, *ziski);
-	mopo->use(4, *ziski);
-	ovi.use(0, *mopo);
-	ziski->use(1, ovi);
-
-	delete src;
-	delete mopo;
-	delete ziski;
-}
-
 int main()
 {
 	IMateriaSource* src = new MateriaSource();
@@ -128,21 +40,18 @@ int main()
 	me->use(10, *me);
 
 	std::cout << "\n============Test character deep copy==========" << std::endl << std::endl;
-	ICharacter* cloud = new Character("clou");
-	ICharacter* seph = new Character("seph");
+	Character* cloud = new Character("clou");
+	Character seph("seph");
+	seph = *cloud;
 	cloud->equip(tmp);
-	cloud->use(0, *seph);
-	seph->use(0, *cloud);
-	delete cloud;
-	delete seph;
+	cloud->use(0, seph);
+	seph.use(0, *cloud);
 
-	std::cout << std::endl << "==============================="  << std::endl;
+	std::cout << std::endl << "============= DELETES =================="  << std::endl;
+	delete cloud;
 	delete bob;
 	delete me;
 	delete src;
 	delete tmp;
 	return 0;
 }
-
-// si haces equip de 1 materia a 2 personajes segfault
-// falta constructor asignacion y copia para Character
