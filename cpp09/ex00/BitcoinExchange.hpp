1ad613eeedd56c68c519da1e61e450d1391ab834
Albert Caballero <albert@Alberts-MacBook-Pro.local>
2024-10-27 23:42:05 +0100

    started cpp09

---
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,11 @@
+#ifndef BITCOIN_H_
+#define BITCOIN_H_
+
+class BitcoinExchange{
+private:
+public:
+	BitcoinExchange();
+	~BitcoinExchange();
+};
+
+#endif
---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "BitcoinExchange.hpp"
+#include <iostream>
+
+int main(int argc, char **argv){
+	if (argc != 2){
+		std::cout << "Only 1 argument accepted\n";
+		return;
+	}
+
+	std::string file(argv[1]);
+}
