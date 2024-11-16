a17d065d58a45c63fbdf558f58337fd6f5d900ac
Albert Caballero <albert@Alberts-MacBook-Pro.local>
2024-11-16 20:17:51 +0100

    asdfasdf

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -1,1 +1,17 @@
 #include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange(){}
+BitcoinExchange::~BitcoinExchange(){}
+
+BitcoinExchange::BitcoinExchange(std::string fname){
+	std::ifstream file;
+	file.open(fname);
+
+	file.close();
+}
+
+float BitcoinExchange::getValue(std::string date){
+	if (_rates.empty())
+		throw (std::out_of_range("no value for that date"));
+	return _rates[0];
+}
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -1,11 +1,19 @@
 #ifndef BITCOIN_H_
 #define BITCOIN_H_
 
+#include <iostream>
+#include <fstream>
+#include <map>
+
 class BitcoinExchange{
 private:
+	std::map<std::string, float> _rates;
 public:
 	BitcoinExchange();
+	BitcoinExchange(std::string);
 	~BitcoinExchange();
+
+	float getValue(std::string);
 };
 
 #endif
